1578ecdb299e0396c197adad0654220f1da32cac
HonEmeritus <136513891+HonEmeritus@users.noreply.github.com>
2025-02-11 12:39:02 +0100

    Add files via upload

---
+++ PROJECT_STAGE_0.py
@@ -0,0 +1,52 @@
+team_info = [
+    {
+        "Name": "Popoola Oluwatobiloba Emmanuel",
+        "Slack Username": "Emeritus",
+        "Email": "oluwatobipelumi.do@gmail.com",
+        "Hobby": "Doing music",
+        "Country": "Nigeria",
+        "Discipline": "Animal Production and Health",
+        "preferred_language": "Python"
+    },
+
+    {
+        "Name": "John Bull",
+        "Slack Username": "Bulldog",
+        "Email": "bulldog.do@gmail.com",
+        "Hobby": "Reading",
+        "Country": "China",
+        "Discipline": "Cell Biology",
+        "preferred_language": "R"
+    },
+
+    {
+        "Name": "Jane Doe",
+        "Slack Username": "Janie",
+        "Email": "janedoe@gmail.com",
+        "Hobby": "Watching movies",
+        "Country": "USA",
+        "Discipline": "Genetics",
+        "preferred_language": "R"
+    },
+
+    {
+        "Name": "Mount Jason",
+        "Slack Username": "Jason",
+        "Email": "jason@gmail.com",
+        "Hobby": "Cooking",
+        "Country": "UK",
+        "Discipline": "Mathematical Science",
+        "preferred_language": "Python"
+    },
+
+    {
+        "Name": "Madison Jake",
+        "Slack Username": "Jakey",
+        "Email": "jakey@gmail.com",
+        "Hobby": "Sleeping",
+        "Country": "Mexico",
+        "Discipline": "Breeding and Genetics",
+        "Preferred Programming Language": "R"
+    }
+]
+print(team_info)
